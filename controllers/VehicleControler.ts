e4c451f35c75593e4e469c73691f640657c683bb
Leandro Murgo <leandrouni01@gmail.com>
2021-11-01 18:01:03 -0300

    feat(controllers) create vehicle controller CRUD methods

---
+++ controllers/VehicleControler.ts
@@ -0,0 +1,34 @@
+import { VehicleQuery } from "../queries/VehicleQuery";
+import { IUpdateVehicle } from "../interfaces/VehicleInterfaces";
+import { IVehicle } from "../interfaces/VehicleInterfaces";
+
+
+export class VehicleController {
+
+  private vehicleQueries: VehicleQuery;
+
+  constructor() {
+    this.vehicleQueries = new VehicleQuery();
+  }
+
+  public async getVehicles() {
+    return await this.vehicleQueries.getVehicles();
+  }
+
+  public async getVehicleById(id:number) {
+    return await this.vehicleQueries.getVehicleById(id);
+  }
+
+  public async createVehicle(vehicle:IVehicle){
+    return await this.vehicleQueries.createVehicle(vehicle)
+  }
+
+  public async updateVehicle(vehicle:IUpdateVehicle){
+    return await this.vehicleQueries.updateVehicle(vehicle)
+  }
+
+  public async deleteVehicle(id:number){
+    return await this.vehicleQueries.deleteVehicle(id)
+  }
+
+}
